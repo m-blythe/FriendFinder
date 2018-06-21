82d35d89279dc72a5217347924dcc1e072cc6349
MEBlythe <m_blythe@yahoo.com>
2018-06-21 01:07:55 -0500

    adding friends into file

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,1 +1,60 @@
-
+var friends = [
+    {
+       "name":"Labrador Retriever",
+       "photo":"https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12231410/Labrador-Retriever-On-White-01.jpg",
+       "scores":[5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+       "name":"Spinone Italiano",
+       "photo":"https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12223827/Spinone-Italiano-On-White-02.jpg",
+       "scores":[4,2,5,1,3,2,2,1,3,2]
+    },
+    {
+       "name":"Afghan Hound",
+       "photo":"https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12221749/afhagn-3.jpg",
+       "scores":[5,2,2,2,4,1,3,2,5,5]
+    },
+    {
+       "name":"Basset Hound",
+       "photo":"https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001049/Basset-Hound-On-White-01.jpg",
+       "scores":[3,3,4,2,2,1,3,2,2,3]
+    },
+     {
+        "name":"Bichon Frise",
+        "photo":"https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13000411/Bichon-Frise-On-White-01.jpg",
+        "scores":[4,3,4,1,5,2,5,3,1,4]
+     },
+       {
+       "name":"Petite Basset Griffon Vendeen",
+       "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Aatef_Jenyat.jpg/293px-Aatef_Jenyat.jpg",
+       "scores":[3,3,4,1,4,3,5,3,5,4]
+    },
+     {
+        "name":"Mastiff",
+        "photo":"http://www.billboard.com/files/styles/article_main_image/public/stylus/1235529-taylor-swift-woman-of-the-year-617-409.jpg",
+        "scores":[4,4,2,3,2,2,3,2,4,5]
+     },
+     {
+         "name": "doggy",
+         "photo":"http://www.billboard.com/files/styles/article_main_image/public/stylus/1235529-taylor-swift-woman-of-the-year-617-409.jpg",
+         "scores":[4,4,2,3,2,2,3,2,4,5]
+     },
+     {
+         "name": "doggy",
+         "photo":"http://www.billboard.com/files/styles/article_main_image/public/stylus/1235529-taylor-swift-woman-of-the-year-617-409.jpg",
+         "scores":[4,4,2,3,2,2,3,2,4,5]
+     },
+     {
+         "name": "doggy",
+         "photo":"http://www.billboard.com/files/styles/article_main_image/public/stylus/1235529-taylor-swift-woman-of-the-year-617-409.jpg",
+         "scores":[4,4,2,3,2,2,3,2,4,5]
+     },
+     {
+         "name": "doggy",
+         "photo":"http://www.billboard.com/files/styles/article_main_image/public/stylus/1235529-taylor-swift-woman-of-the-year-617-409.jpg",
+         "scores":[4,4,2,3,2,2,3,2,4,5]
+     }
+ 
+ ]
+ 
+ module.exports = friends;
